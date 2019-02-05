7302ffa1f8fce88d4d8327b7986387d62e232078
George Welsted <george@gwelsted.com>
2019-02-05 16:04:47 -0500

    adding a place file to make sure directory is created when repo is cloned
    updated index to not include place file in results removed test file.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function get_result($file)
 }
 
 foreach ($files as $file) {
-    if (($file == ".")||($file == "..")||($file == ".html")||($file==".DS_Store")) {
+    if (($file == ".")||($file == "..")||($file == ".html")||($file==".DS_Store")||($file==".place")) {
         //Do Nothing
     } else {
         echo "<a href='".$dir.$file."''>".$file."</a> ".get_result($file)."</br>";
